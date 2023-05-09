527f0134d33cbebfc13166991b39864ddead5442
Edw4rd0 <e.gaudiosi@studenti.unina.it>
2023-05-09 18:23:00 +0200

    binarytree + bst + iterator.hpp

---
+++ iterator/iterator.hpp
@@ -0,0 +1,240 @@
+
+#ifndef ITERATOR_HPP
+#define ITERATOR_HPP
+
+/* ************************************************************************** */
+
+namespace lasd {
+
+/* ************************************************************************** */
+
+template <typename Data>
+class Iterator {
+
+private:
+
+protected:
+
+public:
+
+  // Destructor
+  virtual ~Iterator() = default;
+
+  /* ************************************************************************ */
+
+  // Copy assignment
+  Iterator& operator=(const Iterator&) = delete; // Copy assignment of abstract types should not be possible.
+
+  // Move assignment
+  Iterator& operator=(Iterator&&) noexcept = delete; // Move assignment of abstract types should not be possible.
+
+  /* ************************************************************************ */
+
+  // Comparison operators
+  bool operator==(const Iterator&) const noexcept = delete; // Comparison of abstract types might not be possible.
+  bool operator!=(const Iterator&) const noexcept = delete; // Comparison of abstract types might not be possible.
+
+  /* ************************************************************************ */
+
+  // Specific member functions
+
+  virtual Data& operator*() const = 0; // (non-mutable version; concrete function must throw std::out_of_range when terminated)
+  virtual bool Terminated() const noexcept = 0; // (concrete function should not throw exceptions)
+
+};
+
+/* ************************************************************************** */
+
+template <typename Data>
+class MutableIterator : virtual public Iterator<Data> {
+
+private:
+
+protected:
+
+public:
+
+  // Destructor
+  virtual ~MutableIterator() = default;
+
+  /* ************************************************************************ */
+
+  // Copy assignment
+  MutableIterator& operator=(const MutableIterator&) = delete; // Copy assignment of abstract types should not be possible.
+
+  // Move assignment
+  MutableIterator& operator=(MutableIterator&&) noexcept = delete; // Move assignment of abstract types should not be possible.
+
+  /* ************************************************************************ */
+
+  // Comparison operators
+  bool operator==(const MutableIterator&) const noexcept = delete; // Comparison of abstract types might not be possible.
+  bool operator!=(const MutableIterator&) const noexcept = delete; // Comparison of abstract types might not be possible.
+
+  /* ************************************************************************ */
+
+  // Specific member functions
+
+  virtual Data& operator*() = 0; // (mutable version; concrete function must throw std::out_of_range when terminated)
+
+};
+
+/* ************************************************************************** */
+
+template <typename Data>
+class ForwardIterator : virtual public Iterator<Data> {
+
+private:
+
+protected:
+
+public:
+
+  // Destructor
+  virtual ~ForwardIterator() = default;
+
+  /* ************************************************************************ */
+
+  // Copy assignment
+  ForwardIterator& operator=(const ForwardIterator&) = delete; // Copy assignment of abstract types should not be possible.
+
+  // Move assignment
+  ForwardIterator& operator=(ForwardIterator&&) noexcept = delete; // Move assignment of abstract types should not be possible.
+
+  /* ************************************************************************ */
+
+  // Comparison operators
+  bool operator==(const ForwardIterator&) const noexcept = delete; // Comparison of abstract types might not be possible.
+  bool operator!=(const ForwardIterator&) const noexcept = delete; // Comparison of abstract types might not be possible.
+
+  /* ************************************************************************ */
+
+  // Specific member functions
+
+  virtual ForwardIterator& operator++() = 0; // (concrete function must throw std::out_of_range when terminated)
+
+};
+
+/* ************************************************************************** */
+
+template <typename Data>
+class BackwardIterator : virtual public Iterator<Data> {
+
+private:
+
+protected:
+
+public:
+
+  // Destructor
+  virtual ~BackwardIterator() = default;
+
+  /* ************************************************************************ */
+
+  // Copy assignment
+  BackwardIterator& operator=(const BackwardIterator&) = delete; // Copy assignment of abstract types should not be possible.
+
+  // Move assignment
+  BackwardIterator& operator=(BackwardIterator&&) noexcept = delete; // Move assignment of abstract types should not be possible.
+
+  /* ************************************************************************ */
+
+  // Comparison operators
+  bool operator==(const BackwardIterator&) const noexcept = delete; // Comparison of abstract types might not be possible.
+  bool operator!=(const BackwardIterator&) const noexcept = delete; // Comparison of abstract types might not be possible.
+
+  /* ************************************************************************ */
+
+  // Specific member functions
+    //To implement as an exercise after the exam
+    //virtual BackwardIterator& operator--() = 0; // (concrete function must throw std::out_of_range when terminated)
+
+};
+
+/* ************************************************************************** */
+
+template <typename Data>
+class BidirectionalIterator : virtual public ForwardIterator<Data>,
+                              virtual public BackwardIterator<Data> {
+
+private:
+
+protected:
+
+public:
+
+  // Destructor
+  virtual ~BidirectionalIterator() = default;
+
+  /* ************************************************************************ */
+
+  // Copy assignment
+  BidirectionalIterator& operator=(const BidirectionalIterator&) = delete; // Copy assignment of abstract types should not be possible.
+
+  // Move assignment
+  BidirectionalIterator& operator=(BidirectionalIterator&&) noexcept = delete; // Move assignment of abstract types should not be possible.
+
+  /* ************************************************************************ */
+
+  // Comparison operators
+  bool operator==(const BidirectionalIterator&) const noexcept = delete; // Comparison of abstract types might not be possible.
+  bool operator!=(const BidirectionalIterator&) const noexcept = delete; // Comparison of abstract types might not be possible.
+
+  /* ************************************************************************ */
+
+  // Specific member functions
+
+  // type Terminated() specifiers; // Override Iterator member
+
+  // type ForwardTerminated() specifiers; // (concrete function should not throw exceptions)
+
+  // type BackwardTerminated() specifiers; // (concrete function should not throw exceptions)
+
+};
+
+/* ************************************************************************** */
+
+template <typename Data>
+class ResettableIterator {
+                            // Must extend Iterator<Data>
+
+private:
+
+  // ...
+
+protected:
+
+  // ...
+
+public:
+
+  // Destructor
+  // ~ResettableIterator() specifiers
+
+  /* ************************************************************************ */
+
+  // Copy assignment
+  // type operator=(argument); // Copy assignment of abstract types should not be possible.
+
+  // Move assignment
+  // type operator=(argument); // Move assignment of abstract types should not be possible.
+
+  /* ************************************************************************ */
+
+  // Comparison operators
+  // type operator==(argument) specifiers; // Comparison of abstract types might not be possible.
+  // type operator!=(argument) specifiers; // Comparison of abstract types might not be possible.
+
+  /* ************************************************************************ */
+
+  // Specific member functions
+
+  // type Reset() specifiers; // (concrete function should not throw exceptions)
+
+};
+
+/* ************************************************************************** */
+
+}
+
+#endif
